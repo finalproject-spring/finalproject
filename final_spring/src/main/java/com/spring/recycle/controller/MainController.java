fb4630cc0142d1101a4936ffad07d704cdc93ee0
jihyeon <jihytt@naver.com>
2021-07-09 01:11:37 +0900

    파일수정

--- final_spring/src/main/java/com/spring/recycle/controller/MainController.java
+++ final_spring/src/main/java/com/spring/recycle/controller/MainController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MainController {
 	
+	// 수정했어요
+	
 	@RequestMapping("/main.do")
 	public String goMain() {
 		return "main/main";
